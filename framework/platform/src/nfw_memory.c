c650dd48acf9418b0f881bde4772e8dcdaac9abc
shreekanthraj-collab <shreekanth.raj@gmail.com>
2026-08-08 13:13:33 +0530

    Implement memory platform abstraction

--- framework/platform/src/nfw_memory.c
+++ framework/platform/src/nfw_memory.c
@@ -1,22 +1,28 @@
 /**
  * @file nfw_memory.c
- * @brief Platform memory abstraction implementation.
+ * @brief ESP-IDF implementation of the Orb Drive memory abstraction.
  */
 
 #include "nfw_memory.h"
 
 #include <stdlib.h>
 
-/*===========================================================================
- * Public Functions
- *===========================================================================*/
-
 void *nfwMalloc(size_t size)
 {
+    if (size == 0U)
+    {
+        return NULL;
+    }
+
     return malloc(size);
 }
 
 void nfwFree(void *ptr)
 {
+    if (ptr == NULL)
+    {
+        return;
+    }
+
     free(ptr);
 }
